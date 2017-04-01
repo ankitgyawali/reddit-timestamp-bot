2d7805cb3aa356585a4d999eab0df81c2d784f73
Ankit Gyawali <dev@ankitgyawali.com>
2017-04-01 14:05:30 -0400

    improve from suggestion

--- Readme.md
+++ Readme.md
@@ -1,3 +1,23 @@
-Reddit bot that parses rising posts with parent comments as youtube url & child replies as timestamps to create grand child comments of timestamped youtube url link .
+### About
+Reddit bot that parses rising posts with parent comments as youtube url & child replies as timestamps to create grand child comments of timestamped youtube url link.
 
 Ran as a cron job every 1 hour.
+
+### Steps to Run (on linux server)
+1. `cp sample-config.ini config.ini` & configure your bot.
+2. `which python` & replace shebang line#1 of main.py with the python path. [Python3 recommended]
+3. `pip install -r requirements.txt` to install python dependencies.
+4.  `crontab -e` & add `main.py` as cronjob at the end of file with following format:
+
+`0 * * * * cd <absolute-path-to-repo>;<absolute-path-to-main.py> >> <aboluste-path-to-cron.log> 2>&1`
+
+Example:
+`0 * * * * cd /var/www/timestamp_bot;/var/www/timestamp_bot/main.py >> /var/www/timestamp_bot/cron.log 2>&1`
+
+### Debugging
+1. `cat /var/log/syslog | grep cron` to check if cron job is running fine.
+2. `cat cron.log` to check for errors from bot/reddit server.
+3. `cat timestamp_bot_log.log` to check bot process/logs.
+
+### Issues/Suggestions
+Report all issues/suggestions at <a href="https://github.com/ankitgyawali/reddit-timestamp-bot/issues" target="_blank">issue page</a>.
--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import json
 import sqlite3
 import logging
 import configparser
+import pyjokes
 
 config = configparser.ConfigParser()
 config.readfp(open(r'config.ini'))
 
-
 # Bot Configs
 YOUTUBE = config.get('YOUTUBE', 'YOUTUBE_API_KEY')
-APIURL = "https://www.googleapis.com/youtube/v3/videos?part=contentDetails&key=" + YOUTUBE + "&id="
+APIURL = "https://www.googleapis.com/youtube/v3/videos?fields=items(id,statistics(likeCount,dislikeCount),contentDetails(duration),snippet(title,channelTitle))&part=snippet,statistics,contentDetails&key=" + YOUTUBE + "&id="
 SLEEPTIME = int(config.get('SLEEPTIME', 'TIME'))
 
 
@@ -31,8 +31,7 @@ cur = sql.cursor()
 cur.execute('CREATE TABLE IF NOT EXISTS timestamp(id TEXT, childid TEXT)')
 cur.execute('CREATE INDEX IF NOT EXISTS postindex on timestamp(id)')
 logging.info('Connected to database.')
-# Subreddits to targets
-subreddit = json.loads(config.get('SUBREDDITS', 'SUBREDDIT_LIST'))
+
 
 reddit = praw.Reddit(client_id=config.get('PRAW_DETAILS', 'CLIENT_ID'),
                      client_secret=config.get('PRAW_DETAILS', 'CLIENT_SECRET'),
@@ -42,7 +41,7 @@ reddit = praw.Reddit(client_id=config.get('PRAW_DETAILS', 'CLIENT_ID'),
 
 # Regex to match when parsing comments for youtube video link & numeric timestamp
 pattern = re.compile("(?:youtube\.com/watch\?v=|youtu.be/)([0-9A-Za-z\-_]*)")
-timepattern = re.compile("(\d{1,2}[\:|\ |\-]\d{1,2}([\:|\ |\-]\d{1,2}]?)?)")
+timepattern = re.compile("(\d{1,2}[\:]\d{1,2}([\:]\d{1,2}]?)?)")
 
 # Save processed comments on database to avoid processing them again
 def addToDatabase(id, childid):
@@ -55,7 +54,7 @@ def isinDatabase(id):
     cur.execute('SELECT * FROM timestamp WHERE id == ?', [id])
     item = cur.fetchone()
     if(item is not None):
-        logging.info('Item id: '+ str(id)+ " is already in database.")
+        logging.info('ID: '+ str(id)+ " found in db!")
     return (item is not None)
 
 # Parse time from extracted comments
@@ -76,7 +75,8 @@ def parsetime(timenum):
 def getLength(url):
     url = APIURL + url
     try:
-        length =  (requests.get(url).json()['items'][0]['contentDetails']['duration'])
+        videoDetails = requests.get(url).json()
+        length =  (videoDetails['items'][0]['contentDetails']['duration'])
         length = length.replace('PT','')
         length = length.replace('H',':')
         length = length.replace('M',':')
@@ -89,54 +89,108 @@ def getLength(url):
             length = [0] + length
     except:
         length = [0,0,0]
-    return length
+        videoDetails = None
+    return [length, videoDetails]
 
 # Check if the requested timestamp value is lesser than youtube video's lenght before replying
 def validate(total,timestamp):
-    return ((total[0]*3600 + total[1]*60 + total[2])>(timestamp[0]*3600+ timestamp[1]*60 + timestamp[2]))
+    if(total[1] is None):
+        logging.info('Something is wrong with youtube API')
+        return False
+    else:
+        return ((total[0][0]*3600 + total[0][1]*60 + total[0][2])>(timestamp[0]*3600+ timestamp[1]*60 + timestamp[2]))
 
 
 # Return a comment to be replied
-def createComment(id,time):
+def createComment(videoDetails,time):
+    popularity = (int(videoDetails['items'][0]['statistics']['likeCount'])+1)/ ((int(int(videoDetails['items'][0]['statistics']['likeCount']))) + (int(int(videoDetails['items'][0]['statistics']['dislikeCount'])))+1) *100
     timestamp = str(time[0]) + "h" + str(time[1]) + "m" + str(time[2]) + "s"
-    logging.info('Comment created for id:'+ id + "on time: "+ str(time))
-    return "https://www.youtube.com/watch?v=" + id + "&t=" + timestamp + "\n \n Reddit Timestamp Bot: [Source Code](https://github.com/ankitgyawali/reddit-timestamp-bot) "
+    logging.info('Comment created for id:'+ videoDetails['items'][0]['id'] + "on time: "+ str(time))
+    response = "[ **Jump to "
+    if(int(time[0]) != 0):
+        response +=  str(time[0]) + ":" + str(time[1]) + ":" + str(time[2])
+    else:
+        response += str(time[1]) + ":" + str(time[2])
+    response += " @** "
+    if(len(videoDetails['items'][0]['snippet']['title']) < 81):
+        response += videoDetails['items'][0]['snippet']['title']
+    else:
+        response += "Referenced Video"
+    response += "](https://www.youtube.com/watch?v="+ (videoDetails['items'][0]['id']) + "&t=" + timestamp + ")"
+    response += "\n \n ^(Video Popularity: " + str("{0:.2f}".format(popularity))+"%"
+    response += ", Channel Name: " + str(videoDetails['items'][0]['snippet']['channelTitle']) + ") \n \n"
+    response += "----------------------------------------------------------------------------- \n \n "
+    response += "^(" + pyjokes.get_joke("en","all").replace('(','[').replace(')',']') + ") \n \n"
+    response += "##**^^Beep ^^Bop, ^^I'm ^^a ^^Time ^^Stamp ^^Bot!** [^^Source ^^Code](https://github.com/ankitgyawali/reddit-timestamp-bot)"
+    return response
+
+def processABatch(batchSubmissions):
+    for submission in batchSubmissions:
+        if (isinDatabase(submission.id)):
+            continue
+        for comment in submission.comments:
+            
+            if isinstance(comment, praw.models.MoreComments): # Bypass for prototype //comment.body & comment.replies
+                continue
+            if (isinDatabase(comment.id)):
+                continue
+            if (len(pattern.findall(submission.url)) == 1) and ((len(timepattern.findall(comment.body)) ==1) and ("youtube.com" not in comment.body) and (("youtu.be" not in comment.body))):
+                if(validate(getLength(pattern.findall(submission.url)[0]), parsetime(timepattern.findall(comment.body)[0]))):
+                    # Make comment and add to db
+                    try:
+                        comment.reply(createComment(getLength(pattern.findall(submission.url)[0])[1],parsetime(timepattern.findall(comment.body)[0])))
+                        addToDatabase(comment.id,submission.id)
+                    except Exception as e:
+                        logging.info('Error occurred while comenting, check logs: '+ str(e))
+                    # print(createComment(getLength(pattern.findall(submission.url)[0])[1],parsetime(timepattern.findall(comment.body)[0])))              
+                    time.sleep(SLEEPTIME)
+                continue
+            else:
+                for sub_comment in comment.replies:
+                    if isinstance(sub_comment, praw.models.MoreComments):
+                        continue
+                    if (isinDatabase(sub_comment.id)):
+                        continue
+                    if (len(pattern.findall(comment.body)) == 1) and ((len(timepattern.findall(sub_comment.body)) ==1) and ("youtube.com" not in sub_comment.body) and (("youtu.be" not in sub_comment.body))):
+                        if(validate(getLength(pattern.findall(comment.body)[0]), parsetime(timepattern.findall(sub_comment.body)[0]))):
+                            # Make comment and add to db
+                            try:
+                                sub_comment.reply(createComment(getLength(pattern.findall(comment.body)[0])[1],parsetime(timepattern.findall(sub_comment.body)[0])))
+                                addToDatabase(sub_comment.id,comment.id)
+                            except Exception as e:
+                                logging.info('Error occurred while comenting, check logs: '+ str(e))  
+                            # print(createComment(getLength(pattern.findall(comment.body)[0])[1],parsetime(timepattern.findall(sub_comment.body)[0])))                                                                            
+                            time.sleep(SLEEPTIME)
+                        continue
+                    else:
+                        for grandchild_comment in sub_comment.replies:
+                            if isinstance(grandchild_comment, praw.models.MoreComments):
+                                continue
+                            if (isinDatabase(grandchild_comment.id)):
+                                continue
+                            if (len(pattern.findall(sub_comment.body)) == 1) and ((len(timepattern.findall(grandchild_comment.body)) ==1) and ("youtube.com" not in grandchild_comment.body) and (("youtu.be" not in grandchild_comment.body))):
+                                if(validate(getLength(pattern.findall(sub_comment.body)[0]), parsetime(timepattern.findall(grandchild_comment.body)[0]))):
+                                    # Make comment and add to db
+                                    try:
+                                        grandchild_comment.reply(createComment(getLength(pattern.findall(sub_comment.body)[0])[1],parsetime(timepattern.findall(grandchild_comment.body)[0])))
+                                        addToDatabase(grandchild_comment.id,sub_comment.id)
+                                    except Exception as e:
+                                        logging.info('Error occurred while comenting, check logs: '+ str(e))
+                                    # print(createComment(getLength(pattern.findall(sub_comment.body)[0])[1],parsetime(timepattern.findall(grandchild_comment.body)[0])))
+                                    time.sleep(SLEEPTIME)
+                                continue
+
+    logging.info('Finished processing submission rising batch')
 
 # Main
-for submission in reddit.subreddit("+".join(subreddit)).rising(limit=int(config.get('SUBREDDITS', 'SUBMISSIONS_TO_PROCESS'))):
-    if (isinDatabase(submission.id)):
-        continue
-    for comment in submission.comments:
-        if isinstance(comment, praw.models.MoreComments): # Bypass for prototype //comment.body & comment.replies
-            continue
-        if (isinDatabase(comment.id)):
-            continue
-        if (len(pattern.findall(submission.url)) == 1) and len(timepattern.findall(comment.body)) ==1:
-            if(validate(getLength(pattern.findall(submission.url)[0]), parsetime(timepattern.findall(comment.body)[0]))):
-                # Make comment and add to db
-                try:
-                    comment.reply(createComment(pattern.findall(submission.url)[0],parsetime(timepattern.findall(comment.body)[0])))
-                    addToDatabase(submission.id, comment.id)
-                except Exception as e:
-                    logging.info('Error occurred while comenting, check logs: '+ str(e))
-                # print(createComment(pattern.findall(submission.url)[0],parsetime(timepattern.findall(comment.body)[0])))                
-                time.sleep(SLEEPTIME)
-            continue
-        else:
-            for sub_comment in comment.replies:
-                if isinstance(sub_comment, praw.models.MoreComments):
-                    continue
-                if (isinDatabase(sub_comment.id)):
-                    continue
-                if (len(pattern.findall(comment.body)) == 1) and len(timepattern.findall(sub_comment.body)) ==1:
-                    if(validate(getLength(pattern.findall(comment.body)[0]), parsetime(timepattern.findall(sub_comment.body)[0]))):
-                        # Make comment and add to db
-                        try:
-                            sub_comment.reply(createComment(pattern.findall(comment.body)[0],parsetime(timepattern.findall(sub_comment.body)[0])))
-                            addToDatabase(comment.id,sub_comment.id)
-                        except Exception as e:
-                            logging.info('Error occurred while comenting, check logs: '+ str(e))  
-                        # print(createComment(pattern.findall(comment.body)[0],parsetime(timepattern.findall(sub_comment.body)[0])))                                                
-                        time.sleep(SLEEPTIME)
-                    continue
-logging.info('Finished processing submission batch')
+# Subreddits to target
+logging.info('New Batch Started: Rising Multireddits')
+processABatch(reddit.subreddit("+".join(json.loads(config.get('SUBREDDITS', 'MULTIREDDIT_LIST')))).rising(limit=int(config.get('SUBREDDITS', 'MULTIREDDIT_POSTS'))))
+logging.info('New Batch Started: Hot Multireddits')
+processABatch(reddit.subreddit("+".join(json.loads(config.get('SUBREDDITS', 'MULTIREDDIT_LIST')))).hot(limit=int(config.get('SUBREDDITS', 'MULTIREDDIT_POSTS'))))
+
+logging.info('New Batch Started: Rising All')
+processABatch(reddit.subreddit("+".join(json.loads(config.get('SUBREDDITS', 'ALL_SUBREDDIT')))).rising(limit=int(config.get('SUBREDDITS', 'ALL_POSTS'))))
+logging.info('New Batch Started: Hot All')
+processABatch(reddit.subreddit("+".join(json.loads(config.get('SUBREDDITS', 'ALL_SUBREDDIT')))).hot(limit=int(config.get('SUBREDDITS', 'ALL_POSTS'))))
+logging.info('Finished Processing')
