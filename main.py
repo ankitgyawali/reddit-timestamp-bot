ea54bba41f4e7b6da09f2d8e91816e2d4407b3eb
ankitgyawali <dev@ankitgyawali.com>
2017-08-22 20:44:32 -0400

    minor updates

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/home/dude/anaconda3/bin/python
+#!/root/miniconda3/bin/python
 import praw
 import time
 import re
@@ -7,7 +7,6 @@ import json
 import sqlite3
 import logging
 import configparser
-import pyjokes
 
 config = configparser.ConfigParser()
 config.readfp(open(r'config.ini'))
@@ -18,11 +17,13 @@ APIURL = "https://www.googleapis.com/youtube/v3/videos?fields=items(id,statistic
 SLEEPTIME = int(config.get('SLEEPTIME', 'TIME'))
 
 
+logging.getLogger("requests").setLevel(logging.WARNING)
 logging.basicConfig(
-    format = '[%(asctime)s] %(levelname)s: %(name)s: %(message)s',
+    format = '[%(asctime)s] %(levelname)s: {%(pathname)s:%(lineno)d} %(name)s: %(message)s',
     filename = config.get('LOGGING', 'LOG_FILENAME'),
 level=logging.INFO)
 
+#logging.fileConfig(disable_existing_loggers=False)
 logging.info('Starting timestamp reddit bot.')
 
 # Initalize & Connect to database to keep track of processed comments
@@ -43,6 +44,14 @@ reddit = praw.Reddit(client_id=config.get('PRAW_DETAILS', 'CLIENT_ID'),
 pattern = re.compile("(?:youtube\.com/watch\?v=|youtu.be/)([0-9A-Za-z\-_]*)")
 timepattern = re.compile("(\d{1,2}[\:]\d{1,2}([\:]\d{1,2}]?)?)")
 
+def changeTime(array):
+    if(array[0]*3600+array[1]*60+array[2] > 4):
+        m, s = divmod((array[0]*3600+array[1]*60+array[2])-5, 60)
+        h, m = divmod(m, 60)
+        return [h,m,s]
+    else:
+        return -1
+
 # Save processed comments on database to avoid processing them again
 def addToDatabase(id, childid):
     logging.info('Added to database :'+ id)
@@ -53,8 +62,8 @@ def addToDatabase(id, childid):
 def isinDatabase(id):
     cur.execute('SELECT * FROM timestamp WHERE id == ?', [id])
     item = cur.fetchone()
-    if(item is not None):
-        logging.info('ID: '+ str(id)+ " found in db!")
+    #if(item is not None):
+    #    logging.info('ID: '+ str(id)+ " found in db!")
     return (item is not None)
 
 # Parse time from extracted comments
@@ -103,26 +112,68 @@ def validate(total,timestamp):
 
 # Return a comment to be replied
 def createComment(videoDetails,time):
-    popularity = (int(videoDetails['items'][0]['statistics']['likeCount'])+1)/ ((int(int(videoDetails['items'][0]['statistics']['likeCount']))) + (int(int(videoDetails['items'][0]['statistics']['dislikeCount'])))+1) *100
+    try:
+        popularity = (int(videoDetails['items'][0]['statistics']['likeCount'])+1)/ ((int(int(videoDetails['items'][0]['statistics']['likeCount']))) + (int(int(videoDetails['items'][0]['statistics']['dislikeCount'])))+1) *100
+        logging.info("Calculated Video Populatiry: "+ str(popularity))
+    except Exception as e:
+        popularity = "F"
+        logging.info("Stat exception:  " + str(e))
+        logging.info("Exception at: "+ json.dumps(videoDetails))
+    #earlier = ''
     timestamp = str(time[0]) + "h" + str(time[1]) + "m" + str(time[2]) + "s"
-    logging.info('Comment created for id:'+ videoDetails['items'][0]['id'] + "on time: "+ str(time))
+    logging.info('Comment created for id:'+ videoDetails['items'][0]['id'] + " on time: "+ str(time))
+    if(changeTime(time)[0] == -1):
+        addToResponse = " "
+    else:
+        addToResponse = "^, [^Jump ^5 ^secs ^earlier ^for ^context ^@"
+        if(int(changeTime(time)[0]) != 0):
+            addToResponse += str(changeTime(time)[0]).rjust(2, '0') + ":" + str(changeTime(time)[1]).rjust(2, '0') + ":" + str(changeTime(time)[2]).rjust(2, '0')
+        else:
+            addToResponse += str(changeTime(time)[1]).rjust(2, '0') + ":" + str(changeTime(time)[2]).rjust(2, '0')
+        addToResponse +=  "](https://www.youtube.com/watch?v="+ (videoDetails['items'][0]['id']) + "&t=" + str(changeTime(time)[0]) + "h" + str(changeTime(time)[1]) + "m" + str(changeTime(time)[2]) + "s" + ")"
+    # response = "https://www.youtube.com/watch?v=" + (videoDetails['items'][0]['id']) + "&t=" + timestamp
+    # response = response + "\n \n Reddit Timestamp Bot: [Source Code](https://github.com/ankitgyawali/reddit-timestamp-bot) "
     response = "[ **Jump to "
     if(int(time[0]) != 0):
-        response +=  str(time[0]) + ":" + str(time[1]) + ":" + str(time[2])
+        response +=  str(time[0]).rjust(2, '0') + ":" + str(time[1]).rjust(2, '0') + ":" + str(time[2]).rjust(2, '0')
     else:
-        response += str(time[1]) + ":" + str(time[2])
+        response += str(time[1]).rjust(2, '0') + ":" + str(time[2]).rjust(2, '0')
     response += " @** "
     if(len(videoDetails['items'][0]['snippet']['title']) < 81):
         response += videoDetails['items'][0]['snippet']['title']
     else:
-        response += "Referenced Video"
+    	response += "Referenced Video"
     response += "](https://www.youtube.com/watch?v="+ (videoDetails['items'][0]['id']) + "&t=" + timestamp + ")"
-    response += "\n \n ^(Video Popularity: " + str("{0:.2f}".format(popularity))+"%"
-    response += ", Channel Name: " + str(videoDetails['items'][0]['snippet']['channelTitle']) + ") \n \n"
+    response += "\n \n ^("
+    response += "Channel Name: " + str(videoDetails['items'][0]['snippet']['channelTitle'])
+    if(popularity != "F"):
+        response += ", Video Popularity: "
+        response +=  str("{0:.2f}".format(popularity))+"%"
+    response += ", Video Length: ["
+    length =  (videoDetails['items'][0]['contentDetails']['duration'])
+    length = length.replace('PT','')
+    length = length.replace('H',':')
+    length = length.replace('M',':')
+    length = length.replace('S','')
+    length = length.split(":")
+    length = [str(splitLength).rjust(2, '0') for splitLength in length]
+    length = ":".join(length)
+    response += length
+    response += "])"
+    response += addToResponse
+    response +=" \n \n"
     response += "----------------------------------------------------------------------------- \n \n "
-    response += "^(" + pyjokes.get_joke("en","all").replace('(','[').replace(')',']') + ") \n \n"
-    response += "##**^^Beep ^^Bop, ^^I'm ^^a ^^Time ^^Stamp ^^Bot!** [^^Source ^^Code](https://github.com/ankitgyawali/reddit-timestamp-bot)"
+    #response += "^(" + pyjokes.get_joke("en","all").replace('(','[').replace(')',']') + ") \n \n"
+    response += "^^Beep ^^Bop, ^^I'm ^^a ^^Time ^^Stamp ^^Bot! [^^Source ^^Code](https://github.com/ankitgyawali/reddit-timestamp-bot)"
+    response += " ^^| [^^Suggestions](https://www.reddit.com/r/timestamp_bot)"
     return response
+
+
+def isTimeStamp(comment):
+    if (len(timepattern.findall(comment))==1) and (len(comment.split(''.join(timepattern.findall(comment)[0]))) ==2):
+        return (not ("am" in comment.split(''.join(timepattern.findall(comment)[0]))[1][:3].lower())) and (not ("pm" in comment.split(''.join(timepattern.findall(comment)[0]))[1][:3].lower()))
+    else:
+        return False
 
 def processABatch(batchSubmissions):
     for submission in batchSubmissions:
@@ -134,7 +185,7 @@ def processABatch(batchSubmissions):
                 continue
             if (isinDatabase(comment.id)):
                 continue
-            if (len(pattern.findall(submission.url)) == 1) and ((len(timepattern.findall(comment.body)) ==1) and ("youtube.com" not in comment.body) and (("youtu.be" not in comment.body))):
+            if (len(pattern.findall(submission.url)) == 1) and ((len(timepattern.findall(comment.body)) ==1) and ("http:" not in comment.body) and ("youtube.com" not in comment.body) and (("youtu.be" not in comment.body))):
                 if(validate(getLength(pattern.findall(submission.url)[0]), parsetime(timepattern.findall(comment.body)[0]))):
                     # Make comment and add to db
                     try:
@@ -151,7 +202,7 @@ def processABatch(batchSubmissions):
                         continue
                     if (isinDatabase(sub_comment.id)):
                         continue
-                    if (len(pattern.findall(comment.body)) == 1) and ((len(timepattern.findall(sub_comment.body)) ==1) and ("youtube.com" not in sub_comment.body) and (("youtu.be" not in sub_comment.body))):
+                    if (len(pattern.findall(comment.body)) == 1) and ((len(timepattern.findall(sub_comment.body)) ==1) and ("http:" not in sub_comment.body) and ("youtube.com" not in sub_comment.body) and (("youtu.be" not in sub_comment.body))):
                         if(validate(getLength(pattern.findall(comment.body)[0]), parsetime(timepattern.findall(sub_comment.body)[0]))):
                             # Make comment and add to db
                             try:
@@ -168,7 +219,7 @@ def processABatch(batchSubmissions):
                                 continue
                             if (isinDatabase(grandchild_comment.id)):
                                 continue
-                            if (len(pattern.findall(sub_comment.body)) == 1) and ((len(timepattern.findall(grandchild_comment.body)) ==1) and ("youtube.com" not in grandchild_comment.body) and (("youtu.be" not in grandchild_comment.body))):
+                            if (len(pattern.findall(sub_comment.body)) == 1) and ((len(timepattern.findall(grandchild_comment.body)) ==1) and ("http:" not in grandchild_comment.body) and ("youtube.com" not in grandchild_comment.body) and (("youtu.be" not in grandchild_comment.body))):
                                 if(validate(getLength(pattern.findall(sub_comment.body)[0]), parsetime(timepattern.findall(grandchild_comment.body)[0]))):
                                     # Make comment and add to db
                                     try:
@@ -186,11 +237,11 @@ def processABatch(batchSubmissions):
 # Subreddits to target
 logging.info('New Batch Started: Rising Multireddits')
 processABatch(reddit.subreddit("+".join(json.loads(config.get('SUBREDDITS', 'MULTIREDDIT_LIST')))).rising(limit=int(config.get('SUBREDDITS', 'MULTIREDDIT_POSTS'))))
-logging.info('New Batch Started: Hot Multireddits')
-processABatch(reddit.subreddit("+".join(json.loads(config.get('SUBREDDITS', 'MULTIREDDIT_LIST')))).hot(limit=int(config.get('SUBREDDITS', 'MULTIREDDIT_POSTS'))))
+#logging.info('New Batch Started: Hot Multireddits')
+processABatch(reddit.subreddit("+".join(json.loads(config.get('SUBREDDITS', 'MULTIREDDIT_LIST')))).hot(limit=int(config.get('SUBREDDITS', 'ALL_POSTS'))))
 
-logging.info('New Batch Started: Rising All')
-processABatch(reddit.subreddit("+".join(json.loads(config.get('SUBREDDITS', 'ALL_SUBREDDIT')))).rising(limit=int(config.get('SUBREDDITS', 'ALL_POSTS'))))
-logging.info('New Batch Started: Hot All')
+#logging.info('New Batch Started: Rising All')
+processABatch(reddit.subreddit("+".join(json.loads(config.get('SUBREDDITS', 'ALL_SUBREDDIT')))).rising(limit=int(config.get('SUBREDDITS', 'MULTIREDDIT_POSTS'))))
+#logging.info('New Batch Started: Hot All')
 processABatch(reddit.subreddit("+".join(json.loads(config.get('SUBREDDITS', 'ALL_SUBREDDIT')))).hot(limit=int(config.get('SUBREDDITS', 'ALL_POSTS'))))
 logging.info('Finished Processing')
